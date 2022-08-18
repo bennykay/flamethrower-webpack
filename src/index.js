71dcfca5a480b0105d591a9c85d622edc750acdf
Benny <github@liebtdich.net>
2022-08-18 22:34:58 +0200

    First init

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import flamethrower from "flamethrower-router";
+
+var flamethrowerRouter = flamethrower({
+  log: false,
+  prefetch: true,
+  pageTransitions: false,
+});
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const { name } = require("./package.json");
+
+module.exports = {
+  output: {
+    filename: `${name}.js`,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+          },
+        ],
+      },
+    ],
+  },
+};
